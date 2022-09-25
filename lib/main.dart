33984b88fe436d9d5978dac94d423e9500fd7f01
Hamza Ahmad <hamzaa.malik194@gmail.com>
2022-09-25 18:25:11 +0500

    Multiple Cubit Example and Cubit Testing
    Added a parallel HomePage that implements multiple cubit states.
    Added Bloc testing for the example.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -16,6 +15,27 @@ class DiceFaceCubit extends Cubit<int?> {
   }
 }
 
+const List<Color> colors = [
+  Colors.red,
+  Colors.green,
+  Colors.blue,
+  Colors.yellow,
+  Colors.purple,
+  Colors.orange,
+];
+
+class ColorCubit extends Cubit<Color> {
+  ColorCubit() : super(Colors.green[300]!);
+
+  void changeColor() {
+    int number;
+    do {
+      number = Random().nextInt(6);
+    } while (number == 0);
+    emit(colors[number]);
+  }
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -25,14 +45,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => DiceFaceCubit(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<DiceFaceCubit>(
+          create: (context) => DiceFaceCubit(),
+        ),
+        BlocProvider<ColorCubit>(
+          create: (context) => ColorCubit(),
+        ),
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const MyHomePage(),
+        home: const MyHomePage2(),
       ),
     );
   }
@@ -69,39 +96,38 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-// class MyHomePage extends StatelessWidget {
-//   const MyHomePage({Key? key}) : super(key: key);
+class MyHomePage2 extends StatelessWidget {
+  const MyHomePage2({Key? key}) : super(key: key);
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: GestureDetector(
-//           onTap: () {},
-//           child: Container(
-//             height: 100,
-//             width: 100,
-//             decoration: BoxDecoration(
-//               borderRadius: BorderRadius.circular(5),
-//               color: Colors.blue,
-//             ),
-//             child: Builder(
-//               builder: (context) {
-//                 final numberState = context.watch<DiceFaceCubit>().state;
-//                 final colorState = context.watch<ColorCubit>().state;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: GestureDetector(onTap: () {
+          context.read<DiceFaceCubit>().rollDice();
+          context.read<ColorCubit>().changeColor();
+        }, child: Builder(builder: (context) {
+          final numberState = context.watch<DiceFaceCubit>().state;
+          final colorState = context.watch<ColorCubit>().state;
 
-//                 return Text(
-//                   numberState != null ? "$numberState" : "click",
-//                   style: TextStyle(color: colorState),
-//                 );
-//               },
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+          return Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: colorState,
+              ),
+              child: Center(
+                child: Text(
+                  numberState != null ? "$numberState" : "click",
+                  style: const TextStyle(color: Colors.black),
+                ),
+              ));
+        })),
+      ),
+    );
+  }
+}
 
 // class MyHomePage extends StatelessWidget {
 //   const MyHomePage({Key? key}) : super(key: key);
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,57 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:bloc_example/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  DiceFaceCubit diceFaceCubit = DiceFaceCubit();
+  ColorCubit colorCubit = ColorCubit();
+
+  blocTest<DiceFaceCubit, int?>(
+    'Changes value of int when '
+    'rollDice() is called successfully.',
+    setUp: () => diceFaceCubit.state,
+    build: () => diceFaceCubit,
+    act: (cubit) => cubit.rollDice(),
+    expect: () {
+      return [0, 1, 2, 3, 4, 5].contains(diceFaceCubit.state)
+          ? [diceFaceCubit.state]
+          : [];
+    },
+    verify: (_) async {
+      () => diceFaceCubit.rollDice();
+    },
+  );
+
+  blocTest<ColorCubit, Color>(
+    'Changes value of color when '
+    'changeColor() is called successfully.',
+    setUp: () => colorCubit.state,
+    build: () => colorCubit,
+    act: (cubit) => cubit.changeColor(),
+    expect: () {
+      return colors.contains(colorCubit.state) ? [colorCubit.state] : [];
+    },
+    verify: (_) async {
+      () => colorCubit.changeColor();
+    },
+  );
+  testWidgets('Bloc Dice smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that our initial state is click.
+    expect(find.text('click'), findsOneWidget);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.text('click'));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that our dice has rolled.
+    expect(find.text('click'), findsNothing);
   });
 }
